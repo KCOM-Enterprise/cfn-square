72d4ded47a7968e9cb386495625e0cdc59b24be6
Paul Burridge <paul.burridge@kcom.com>
2022-01-31 15:09:16 +0000

    Fix JSON loading bug

--- src/main/python/cfn_sphere/file_loader.py
+++ src/main/python/cfn_sphere/file_loader.py
@@ -79,9 +79,9 @@ class FileLoader(object):
 
         try:
             if url.lower().endswith(".json"):
-                return json.loads(file_content, encoding='utf-8')
+                return json.loads(file_content)
             elif url.lower().endswith(".template"):
-                return json.loads(file_content, encoding='utf-8')
+                return json.loads(file_content)
             elif url.lower().endswith(".yml") or url.lower().endswith(".yaml"):
                 yaml.add_multi_constructor(
                     u"",
